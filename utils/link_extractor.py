68cc3ac273fc3bf0b3d7925b31f93eaffa719137
Ibad-stack <ibadrc@gmail.com>
2026-08-09 12:14:08 -0400

    Handle embedded university job boards

--- utils/link_extractor.py
+++ utils/link_extractor.py
@@ -4,29 +4,30 @@ from urllib.parse import urljoin
 
 def extract_links(html: str, base_url: str):
     """
-    Extract all links from a webpage.
-    Returns a list of dictionaries:
-        {
-            "text": "...",
-            "url": "..."
-        }
+    Extract navigational links from a webpage.
+
+    Iframes are included because several university career pages embed their
+    live job board rather than exposing postings as normal anchors.
     """
 
     soup = BeautifulSoup(html, "html.parser")
-
     links = []
 
     for a in soup.find_all("a", href=True):
-
         text = a.get_text(" ", strip=True)
-
         href = urljoin(base_url, a["href"])
+        links.append({
+            "text": text,
+            "url": href,
+            "type": "anchor",
+        })
 
-        links.append(
-            {
-                "text": text,
-                "url": href
-            }
-        )
+    for iframe in soup.find_all("iframe", src=True):
+        href = urljoin(base_url, iframe["src"])
+        links.append({
+            "text": "Embedded career/job board",
+            "url": href,
+            "type": "iframe",
+        })
 
     return links
